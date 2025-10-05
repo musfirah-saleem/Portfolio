abfe2160c6ea9c6bfc2ad10e2ffd5da0cb576c68
musfirah-saleem <musfirahsaleem606@gmail.com>
2025-10-06 01:42:35 +0500

    chanongs done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -41,7 +41,6 @@ export default function Footer() {
                 <Phone className="w-4 h-4" />
                 +92 336 748 2013
               </a>
-              
               <a
                 href="mailto:business@ahmadamz.com"
                 className="flex items-center gap-3 text-secondary/90 hover:text-gray-400 transition"
